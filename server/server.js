2a03d517d476b3b03f7a3c728efdc4318a8f165b
J. Mark Stevens <github@jmarkstevens.com>
2018-09-22 11:35:53 -0700

    Remove dead lines

--- server/server.js
+++ server/server.js
@@ -12,19 +12,9 @@ const corsOptions = {
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
-// app.use(cors());
-// graphQLServer.use('/graphql', cors(), bodyParser.json(), graphqlExpress({ schema }));
 const server = new ApolloServer(schema);
 server.applyMiddleware({ app });
 
 app.listen({ port: GRAPHQL_PORT }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 )
-// graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-// graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
-
-// graphQLServer.listen(GRAPHQL_PORT, () =>
-//   console.log(
-//     `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`
-//   )
-// );
